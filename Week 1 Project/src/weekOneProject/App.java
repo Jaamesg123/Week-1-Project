c8ae90495ac6f5433ffaf7207324dc9ff3ca5ba2
James Gallaher <James Gallaher@DESKTOP-A287SQA>
2019-10-26 22:38:58 -0700

    First Commit
    Uploading all files

---
+++ Week 1 Project/src/weekOneProject/App.java
@@ -0,0 +1,94 @@
+package weekOneProject;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double goldenApple = 46.89;
+	    
+		double goldenEgg = 53.11;
+		
+		int jacobsWallet = 100;
+		
+		int andrewsWallet = 62;
+		
+		int trueFriends = 5;
+		
+		int fakeFriends = 23;
+		
+		int jacobsAge = 23;
+		
+		int jacobsAgeDifference = 12;
+		
+		int andrewsAge = 18;
+		
+		int andrewsAgeDifference = 17;
+		
+		String jacobFirst = "Jacob ";
+				
+		String jacobMiddle = "D ";		
+				
+		String jacobLast = "Reed";
+		
+		String andrewFirst = "Andrew ";
+				
+		String andrewMiddle = "J ";
+		
+		String andrewLast = "Frye"; {
+	
+			System.out.println(jacobFirst + jacobMiddle + jacobLast + " has 100 dollars"); {
+			
+			}
+					
+			System.out.println(andrewFirst + andrewMiddle + andrewLast + " has 62 dollars"); {
+			
+			}
+			
+			System.out.println("The Golden Apple costs " + goldenApple + " in dollars"); {
+			
+			}
+			
+			System.out.println("The Golden Egg costs " + goldenEgg + " in dollars"); {	
+			
+			}
+			
+			System.out.println(andrewsWallet - goldenApple + " dollars is how much money Andrew has left after purchasing the Golden Apple"); {
+			
+			}
+			
+			System.out.println(jacobsWallet - goldenEgg - goldenApple + " dollars is how much money Jacob has left after purchasing the Golden Egg and the Golden Apple"); {	
+			
+			}
+			
+			System.out.println("Once Andrew was 35 he made " + trueFriends + " friends every year"); {
+			}
+			
+			System.out.println("Once Jacob was 35 he made " + fakeFriends + " friends every year"); {
+				
+			}
+			
+			System.out.println("The total amount of real friends when Andrew was 35 years old was " + andrewsAgeDifference * trueFriends); {
+				
+			}
+			
+			System.out.println("The total amount of fake friends Jacob had when he was 35 years old was " + jacobsAgeDifference * fakeFriends ); {
+				
+			}
+			
+			System.out.println("No matter if you're " + jacobsAge + " years old like " + jacobFirst + jacobMiddle + jacobLast + ", or " + andrewsAge + " years old like " + andrewFirst + andrewMiddle + andrewLast + ", we can all infer that money doesn't buy true friends"); {
+				
+			}
+	
+		}
+		
+		
+				
+	}
+
+	private static String jaLast(String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
